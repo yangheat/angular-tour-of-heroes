191c56f0cce32dca57ec1b8e1eb1edfa960b33e6
sunpark <yangheat@gmail.com>
2021-04-07 21:29:58 +0900

    Change the applictaion app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  // Change the applictaion app
+  title = 'Tour Of Heroes';
 }
