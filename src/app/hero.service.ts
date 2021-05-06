5a007bb9aeec5c6a8ca5c62129356c878a601d2b
sunpark <yangheat@gmaill.com>
2021-05-06 14:06:32 +0900

    Hero Detail page
    https://angular.kr/tutorial/toh-pt5#%ED%9E%88%EC%96%B4%EB%A1%9C-%EC%83%81%EC%84%B8%EC%A0%95%EB%B3%B4-%ED%99%94%EB%A9%B4%EC%9C%BC%EB%A1%9C-%EC%A0%84%ED%99%98%ED%95%98%EA%B8%B0

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,10 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
+  getHero(id : number): Observable<Hero> {
+    // TODO: 이 메시지는 서버에서 히어로 정보를 가져온 후에 보내야합니다.
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
